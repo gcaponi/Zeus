5535f442443483451e3e38c7c31491d13b49f58a
Guglielmo Caponi <guilherme.caponi@gmail.com>
2026-06-10 13:02:12 +0200

    fix: add VPS IP to ALLOWED_HOSTS, use standard PostgreSQL backend

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,13 +2,13 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [".zeus.cais.uno"]
+ALLOWED_HOSTS = [".zeus.cais.uno", "91.230.110.7", "localhost", "127.0.0.1"]
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django_tenants.postgresql_backend",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
