0e829dcef96fda97fe0f9eeeec301a51484531f2
Guglielmo Caponi <guilherme.caponi@gmail.com>
2026-06-10 12:30:04 +0200

    fix: manage.py syntax errors

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
-#!/usr/bin/en python
+#!/usr/bin/env python
 import os
 import sys
 
-def main()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+
+def main():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
